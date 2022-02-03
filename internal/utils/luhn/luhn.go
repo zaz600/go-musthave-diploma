9ffa2a9c8aa136a1fe755908084657f99d60349b
Dmitriy Denisov <zaz600@gmail.com>
2022-02-03 23:22:21 +0300

    fix luhn

--- internal/utils/luhn/luhn.go
+++ internal/utils/luhn/luhn.go
@@ -3,14 +3,15 @@ package luhn
 func CheckLuhn(s string) bool {
 	var sum uint8 = 0
 	for i := len(s) - 1; i >= 0; i-- {
+		pos := len(s) - 1 - i
 		digit := s[i] - '0'
 		switch {
-		case i%2 == 0:
+		case pos%2 == 0:
 			sum += digit
 		case digit < 5:
 			sum += 2 * digit
 		default:
-			sum += digit*2 - 9
+			sum += 2*digit - 9
 		}
 	}
 	return sum%10 == 0
--- internal/utils/luhn/luhn_test.go
+++ internal/utils/luhn/luhn_test.go
@@ -8,6 +8,7 @@ import (
 
 func TestCheckLuhn(t *testing.T) {
 	assert.True(t, CheckLuhn("12345678903"))
+	assert.True(t, CheckLuhn("683458"))
 	assert.False(t, CheckLuhn("92345678903"))
 	assert.False(t, CheckLuhn("sdfsdfd"))
 }
